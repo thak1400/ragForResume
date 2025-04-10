1cd27ab42567f8d5683f090f4c09ab03064ad2de
thak1400 <thak1400@mylaurier.ca>
2025-04-10 02:30:43 -0400

    Comitting updates for chunking

--- app.py
+++ app.py
@@ -1,24 +1,101 @@
 import streamlit as st
-from ragChain import funcDefRagChain, funcExtractPDFText,funcGenResponse
+from ragChain import funcDefRagChainChroma, funcExtractPDFText,funcGenResponse,funcChunkTxtToDoc, funcDefFAISSRetriever, funcDefChromaRetriever, funBuildRAGChain
 from langchain.schema import Document
+import logging
+import os
+from langsmith import Client
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage,AIMessage
+from openai import OpenAI
+from langchain_anthropic import ChatAnthropic
+from langchain.retrievers import BM25Retriever, EnsembleRetriever
+
+logging.basicConfig(level=logging.INFO)
+
+#environment variable
+os.environ["LANGSMITH_TRACING_V2"] = "true"
+os.environ["LANGSMITH_API_KEY"] = "add-your-api-key"
+os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
+os.environ["LANGSMITH_PROJECT"]="ragAppForDocs"
+os.environ["OPENAI_API_KEY"] = "add-your-api-key"
+
 
 def main():
     
     st.title("Resume Assistant")
     
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history=[]
+
     fileUploaded=st.file_uploader("Upload your resume (PDF)", type="pdf")
     
-    if fileUploaded is not None:
-        strResRawTxt=funcExtractPDFText(fileUploaded)
-        docFormatted=[Document(page_content=strResRawTxt)]
+    stVectorChoice=st.radio("Choose Retriever",options=["ChromaDB","FAISS","BM25Retriever","Ensemble"], index=0)
+    stLLM=st.radio("Choose LLM",options=["Claude3","ChatGPT"], index=0)
 
-        objRagChain=funcDefRagChain(docFormatted)
+    if fileUploaded is not None:
+
+        #extracting text from pdf
+        strResRawTxt=funcExtractPDFText(fileUploaded)
+        print(strResRawTxt)
+        
+        # converting raw text to chunks
+        docForVectorStores=funcChunkTxtToDoc(strResRawTxt)
+
+        # selecting retriever based on user's choice
+        if stVectorChoice=="FAISS":
+            objRetriever=funcDefFAISSRetriever(docForVectorStores)
+        elif stVectorChoice=="ChromaDB":
+            objRetriever=funcDefChromaRetriever(docForVectorStores)
+        elif stVectorChoice=="BM25Retriever":
+            objRetriever=BM25Retriever.from_documents(docForVectorStores)
+        else:
+            objRetriever=EnsembleRetriever(
+                retrievers=[funcDefFAISSRetriever(docForVectorStores),funcDefChromaRetriever(docForVectorStores), BM25Retriever.from_documents(docForVectorStores)],
+                weights=[0.34,0.33,0.33]
+                )
+            
+        #custom LC prompt for resumes
+        objResAstPrompt=Client().pull_prompt("rag-for-resume1")
+
+        # selecting LLM based on user's choice
+        if stLLM=="ChatGPT":
+            objLLM=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        else:
+            objLLM=ChatAnthropic(model="claude-3-opus-20240229", api_key="add-your-api-key")
+
+        objRagChain=funBuildRAGChain(objRetriever, objResAstPrompt, objLLM)
+        
+        # #formatting for Chroma
+        # docFormatted=[Document(page_content=strResRawTxt)]
+
+        # #creating chain
+        # objRagChain=funcDefRagChainChroma(docFormatted,objResAstPrompt,objLLM)
+        
+        # displaying chat history
+        for message in st.session_state.chat_history:
+            if isinstance(message,HumanMessage):
+                with st.chat_message("user"):
+                    st.write(message.content)
+            elif isinstance(message,AIMessage):
+                with st.chat_message("assistnant"):
+                    st.write(message.content)
 
         strQuestion=st.text_input("Ask about the resume:")
 
         if strQuestion:
+            
+            # appending user's asked question to chat history
+            st.session_state.chat_history.append(HumanMessage(content=strQuestion))
+            with st.chat_message("user"):
+                st.write(strQuestion)
+
+            # querying RAG Chain
             strAnswer=funcGenResponse(strQuestion,objRagChain)
-            st.write("Answer:",strAnswer)
+
+            # appending AI's answers to chat history
+            st.session_state.chat_history.append(AIMessage(content=strAnswer))        
+            with st.chat_message("assistant"):
+                st.write(strAnswer)
 
 if __name__=="__main__":
     main()
--- ragChain.py
+++ ragChain.py
@@ -1,49 +1,69 @@
-from langchain.prompts import ChatPromptTemplate
-from langchain_openai import ChatOpenAI
-from langsmith import Client
+# from langchain.prompts import ChatPromptTemplate
 from langchain import hub
 from langchain_openai import OpenAIEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores import Chroma, FAISS
 from langchain.chains import create_retrieval_chain
-import os
 import fitz
+import logging
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.schema import Document
 
-os.environ["LANGSMITH_TRACING_V2"] = "true"
-os.environ["LANGSMITH_ENDPOINT"] = "https://api.smith.langchain.com"
-os.environ["LANGSMITH_API_KEY"] = "key"
-os.environ["LANGSMITH_PROJECT"]="ragAppForDocs"
-os.environ["OPENAI_API_KEY"] = "key"
+logging.basicConfig(level=logging.INFO)
 
-# objLCPrompt=hub.pull("rlm/rag-prompt")
-# objCustPrompt=ChatPromptTemplate.from_template("Context:\n{context}\n\nQuestion:\n{question}\n\nAnswer:")
-objResAstPrompt=Client().pull_prompt("rag-for-resume1")
-
-objLLM=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-
-def funcExtractPDFText(strPDFPath):
+# function to extract text from pdfs
+def funcExtractPDFText(filePDF):
     
     with open("tempResume.pdf","wb") as f:
-        f.write(strPDFPath.getbuffer())
-
+        f.write(filePDF.getbuffer())
     strExtractedText=""
     filePDF=fitz.open("tempResume.pdf")
     for pg in filePDF:
         strExtractedText+=pg.get_text("text")
     return strExtractedText
 
-def funcDefRagChain(objDoc):
+# function to chunk pdf text to Document format for vector stores
+def funcChunkTxtToDoc(instrResRawTxt):
 
+    objSplitter=RecursiveCharacterTextSplitter(chunk_size=1500, chunk_overlap=150, separators=["\n\n","\n","●","."," "])
+    chunkedTxt=objSplitter.split_text(instrResRawTxt)
+    docForVectorStore=[Document(page_content=chunk) for chunk in chunkedTxt]
+    return docForVectorStore
+
+# function to create FAISS retriever
+def funcDefFAISSRetriever(indocForVectorStores):
+    objFAISSVectorStore=FAISS.from_documents(documents=indocForVectorStores, embedding=OpenAIEmbeddings(model="text-embedding-3-large"))
+    return objFAISSVectorStore.as_retriever(search_type="mmr")
+
+# function to create ChromaDB retriever
+def funcDefChromaRetriever(indocForVectorStores):
+    objChromaVectorStore=Chroma.from_documents(documents=indocForVectorStores, embedding=OpenAIEmbeddings())
+    return objChromaVectorStore.as_retriever(search_type="mmr")
+
+# function to build RAG Chain using retriever, prompt, and llm
+def funBuildRAGChain(inObjRetriever, inObjPrompt, inObjLLM):
+    objRAGChain=create_retrieval_chain(
+        retriever=inObjRetriever,
+        combine_docs_chain=inObjPrompt | inObjLLM
+    )
+    return objRAGChain
+
+# function to create Chroma vector store and Rag Chain
+def funcDefRagChainChroma(objDoc,inObjPrompt,inObjLLM):
     objVectorStore=Chroma.from_documents(documents=objDoc, embedding=OpenAIEmbeddings())
     objRetriever=objVectorStore.as_retriever()
     objRagChain=create_retrieval_chain(
             retriever=objRetriever,
-            combine_docs_chain=objResAstPrompt | objLLM
+            combine_docs_chain=inObjPrompt | inObjLLM
             )
     return objRagChain
 
+# function to invoke rag chain on question(s) asked
 def funcGenResponse(inStrQuestion, inObjRagChain):
-    
+
+    logging.info(f"Question to GPT:{inStrQuestion}")
     dictResponse=inObjRagChain.invoke({"input":inStrQuestion})
+    
+    logging.info(f"Response by GPT:{dictResponse['answer'].content}")
     return dictResponse["answer"].content
     
 
